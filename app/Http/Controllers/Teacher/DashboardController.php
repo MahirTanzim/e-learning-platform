5c5531a1bb371c6f275cfb81ca6f01f2b8f17841
your-senpai-swapnil <swapnil15-3477@diu.du.bd>
2025-08-14 19:57:49 +0600

    teacher controller created without code

---
+++ app/Http/Controllers/Teacher/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Teacher;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use App\Models\Enrollment;
+use App\Models\BlogPost;
+use App\Models\User;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+
+
+        // controller code loading
+    }
+}
