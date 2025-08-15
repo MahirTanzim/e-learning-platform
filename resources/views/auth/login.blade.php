6ed33afd87eb1439fbd0372ff1bf7a2691c3f1ff
Miraj489 <rahman15-4893@diu.edu.bd>
2025-08-15 23:20:26 +0600

    updated Auth Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,7 +22,19 @@
                 @enderror
             </div>
             
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control @error('password') is-invalid @enderror" 
+                       id="password" name="password" required>
+                @error('password')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
             
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="remember" name="remember">
+                <label class="form-check-label" for="remember">Remember me</label>
+            </div>
             
             <button type="submit" class="btn btn-primary w-100 mb-3">
                 <i class="fas fa-sign-in-alt me-2"></i>Sign In
